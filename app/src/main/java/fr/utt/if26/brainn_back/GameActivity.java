f54f2b4fc56a47c79f122abc296cfd8efc534e4d
gingo21 <francois.reymond10@gmail.com>
2018-10-28 11:44:52 +0100

    Layout grille 9 carrés - Nouvelle activité (2)

---
+++ app/src/main/java/fr/utt/if26/brainn_back/GameActivity.java
@@ -0,0 +1,13 @@
+package fr.utt.if26.brainn_back;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
