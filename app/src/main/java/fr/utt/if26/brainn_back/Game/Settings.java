6bef93c4efbe4cec63fa8d43f43f9334844d945f
MATTHIEULENISSARTDU0 <matt.nissart@hotmail.fr>
2018-12-02 20:12:52 +0100

    Add pop up après la partie

--- app/src/main/java/fr/utt/if26/brainn_back/Game/Settings.java
+++ app/src/main/java/fr/utt/if26/brainn_back/Game/Settings.java
@@ -3,7 +3,7 @@ package fr.utt.if26.brainn_back.Game;
 public class Settings {
     private int niveau = 2;
     private long temps = 3000;
-    private int nbreItems = 10;
+    private int nbreItems = 3;
 
     public Settings(int niveau, long temps, int nbreItems) {
         this.niveau = niveau;
