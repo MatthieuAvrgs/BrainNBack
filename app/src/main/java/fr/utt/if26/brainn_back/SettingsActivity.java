ddebba0b764bfc42751d2a01480e431935e414b4
MATTHIEULENISSARTDU0 <matt.nissart@hotmail.fr>
2018-12-09 19:00:36 +0100

    update settings, display the selected value for boolean & add selection du nb d'items

--- app/src/main/java/fr/utt/if26/brainn_back/SettingsActivity.java
+++ app/src/main/java/fr/utt/if26/brainn_back/SettingsActivity.java
@@ -3,6 +3,7 @@ package fr.utt.if26.brainn_back;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
@@ -16,11 +17,26 @@ public class SettingsActivity extends PreferenceActivity {
     public final static String NIVEAU_ENREGISTRE = "niveau";
     public final static String AVEC_SON = "son";
     public final static String AVEC_COULEUR = "couleur";
+    public final static String NB_CARRES_ENREGISTRE = "nbCarres";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
         SharedPreferences SP = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        String niveauS = SP.getString("niveau","1");
+        String tempsS = SP.getString("temps","2000");
+        String nbCarres = SP.getString("nbCarres","10");
+        boolean isSon = SP.getBoolean("son", false);
+        boolean isCouleur = SP.getBoolean("couleur",false);
+        int niveau = Integer.parseInt(niveauS);
+        int temps = Integer.parseInt(tempsS);
+        int nbreItems = Integer.parseInt(nbCarres);
+
+        SharedPreferences.Editor prefs = PreferenceManager.getDefaultSharedPreferences(this).edit();
+        prefs.putBoolean("son",isSon);
+        prefs.putBoolean("couleur",isCouleur);
+        //prefs.putStringSet("niveau",niveau);
+        prefs.commit();
     }
 
     public static class MyPreferenceFragment extends PreferenceFragment
